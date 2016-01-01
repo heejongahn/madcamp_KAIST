53e6b299976ada43885d60a6a7ab78ea5f523004
Heejong Ahn <heejongahn@gmail.com>
2016-01-01 16:32:12 +0900

    People count functionality (using dummy server)

--- project2/app/src/main/java/com/example/nobell/madagora/PeopleCountFragment.java
+++ project2/app/src/main/java/com/example/nobell/madagora/PeopleCountFragment.java
@@ -1,25 +1,31 @@
 package com.example.nobell.madagora;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PeopleCountFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PeopleCountFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PeopleCountFragment extends Fragment {
-    private String mParam1;
-    private String mParam2;
+    private static final String DEBUG_TAG = "DEBUG_madagora";
+    private static final String SERVER_URL = "http://bit.sparcs.org:8282";
 
     public static PeopleCountFragment newInstance() {
         PeopleCountFragment fragment = new PeopleCountFragment();
@@ -38,48 +44,74 @@ public class PeopleCountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_people_count, container, false);
     }
 
-    /*
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new GetPeopleCountTask().execute();
+        } else {
+            Log.d(DEBUG_TAG, "Connection Failure");
         }
     }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
+    private class GetPeopleCountTask extends AsyncTask<Void, Void, String> {
+        protected String doInBackground(Void... voids) {
+            try {
+                return getPeopleCount();
+            } catch (IOException e) {
+                return "Not Connected to Internet.";
+            }
+        }
+
+        protected void onPostExecute(String result) {
+            TextView countView = (TextView) getView().findViewById(R.id.people_count);
+            countView.setText(result);
+            Log.i(DEBUG_TAG, "SUCCESS");
+        }
+    }
+
+
+    private String getPeopleCount() throws IOException{
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
         try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
+            URL url = new URL(SERVER_URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int response = conn.getResponseCode();
+            is = conn.getInputStream();
+
+            String count = readIt(is, len);
+            return String.format("현재 실습실에는 %d명이 있습니다!", count);
+        } finally{
+            if (is != null) {
+                is.close();
+            }
         }
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+
+        int bodyLength = reader.read(buffer);
+        char[] result = new char[bodyLength];
+        System.arraycopy(buffer, 0, result, 0, bodyLength);
+        return new String(result);
     }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    } */
 
 }
