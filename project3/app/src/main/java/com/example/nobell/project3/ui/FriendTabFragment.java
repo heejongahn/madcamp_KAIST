2f043eca36c326e4392ee99d482597bd8340016a
Heejong Ahn <heejongahn@gmail.com>
2016-01-09 21:03:26 +0900

    integrated FriendAdpater into fragment file

--- project3/app/src/main/java/com/example/nobell/project3/ui/FriendTabFragment.java
+++ project3/app/src/main/java/com/example/nobell/project3/ui/FriendTabFragment.java
@@ -3,6 +3,8 @@ package com.example.nobell.project3.ui;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -10,7 +12,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.activeandroid.query.Select;
 import com.example.nobell.project3.R;
@@ -23,8 +28,8 @@ import java.util.List;
 public class FriendTabFragment extends Fragment {
 
     Context mContext;
-    private ListView m_ListView;
-    private FriendTabCustomListView adapter;
+    private ListView mListView;
+    private FriendAdapter mAdapter;
 
     public FriendTabFragment() {
     }
@@ -41,13 +46,13 @@ public class FriendTabFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_friend_tab,null);
 
-        m_ListView = (ListView) view.findViewById(R.id.listView);
+        mListView = (ListView) view.findViewById(R.id.listView);
         List<Friend> friends = new Select().all().from(Friend.class).execute();
 
-        adapter = new FriendTabCustomListView(getContext(),R.layout.friend_combined_listview, friends);
-        adapter.notifyDataSetChanged();
+        mAdapter = new FriendAdapter(getContext(),R.layout.friend_combined_listview, friends);
+        mAdapter.notifyDataSetChanged();
 
-        m_ListView.setAdapter(adapter);
+        mListView.setAdapter(mAdapter);
         //m_ListView.setOnItemClickListener(mItemClickListener);
 
         return view;
@@ -60,5 +65,49 @@ public class FriendTabFragment extends Fragment {
             // TODO Add this events
         }
     };
+
+
+    public class FriendAdapter extends BaseAdapter {
+        private LayoutInflater inflater;
+        private Context mContext;
+        private int mLayout;
+        private List<Friend> mFriends;
+
+        public FriendAdapter(Context context, int layout, List<Friend> friends){
+            this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            this.mContext = context;
+            this.mLayout = layout;
+            this.mFriends = friends;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = inflater.inflate(mLayout, parent, false);
+            }
+
+            Friend friend = mFriends.get(position);
+
+            ImageView photo = (ImageView) convertView.findViewById(R.id.friend_image);
+            if (friend.getPhoto() == null) {
+                Bitmap image = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.basic_profile);
+                photo.setImageBitmap(image);
+            } else {
+                photo.setImageBitmap(friend.getPhoto());
+            }
+            TextView name = (TextView) convertView.findViewById(R.id.friend_name);
+            name.setText(friend.name);
+
+            return convertView;
+
+        }
+
+        @Override
+        public int getCount(){return mFriends.size();}
+        @Override
+        public String getItem(int position){return null;}
+        @Override
+        public long getItemId(int position){return position;}
+    }
 
 }
