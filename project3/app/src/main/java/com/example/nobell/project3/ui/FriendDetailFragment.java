5c9cc2ffa77adab5bd59fce95c1301648aae3ed2
namkyu2 <knk1002@kaist.ac.kr>
2016-01-11 14:32:37 +0900

    add total event count and last meet date in friend detail tab

--- project3/app/src/main/java/com/example/nobell/project3/ui/FriendDetailFragment.java
+++ project3/app/src/main/java/com/example/nobell/project3/ui/FriendDetailFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
@@ -23,6 +24,10 @@ import com.example.nobell.project3.dataset.Event;
 import com.example.nobell.project3.dataset.Friend;
 import com.example.nobell.project3.dataset.Tag;
 
+import java.text.DateFormat;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 public class FriendDetailFragment extends Fragment {
@@ -65,16 +70,39 @@ public class FriendDetailFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_friend_detail, container, false);
 
+        ListView listView = (ListView) view.findViewById(R.id.friend_detail_listview);
+        List<Event> events = friend.getEvents();
+        listView.setAdapter(new EventAdapter(this.getActivity(), R.layout.event_item, events));
+
         ImageView photo = (ImageView) view.findViewById(R.id.friend_detail_image);
         photo.setImageBitmap(friend.getPhoto());
 
         TextView text = (TextView) view.findViewById(R.id.friend_detail_name);
         text.setText("이름 : " + friend.name);
 
-        ListView listView = (ListView) view.findViewById(R.id.friend_detail_listview);
-        List<Event> events = friend.getEvents();
-        listView.setAdapter(new EventAdapter(this.getActivity(), R.layout.event_item, events));
+        TextView count = (TextView) view.findViewById(R.id.friend_detail_event_count);
+        count.setText("총 글 갯수 : " + String.valueOf(events.size()));
+
+        Date date = new Date(FindLastMeet(events));
+        DateFormat mediumFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+
+        TextView last_meet = (TextView) view.findViewById(R.id.friend_detail_last_meet);
+        last_meet.setText("가장 최근 만난 날 : " + mediumFormat.format(date));
 
         return view;
     }
+
+    private Long FindLastMeet(List<Event> input_events)
+    {
+        Collections.sort(input_events, new Comparator<Event>() {
+            @Override
+            public int compare(Event lhs, Event rhs) {
+                return lhs.date > rhs.date ? -1 : lhs.date < rhs.date ? 1:0;
+            }
+        });
+
+        return input_events.get(0).date;
+
+    }
+
 }
