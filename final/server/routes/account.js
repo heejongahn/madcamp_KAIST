1ecbdfb3a7b3f330728d7b571928dee8cda7e9fa
Heejong Ahn <heejongahn@gmail.com>
2016-01-18 19:59:06 +0900

    temporary signin page

--- final/server/routes/account.js
+++ final/server/routes/account.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/signin', function(req, res, next) {
-  res.render('signin', { title: 'Sign In' });
-});
+router.route('/signin')
+  .get(function(req, res, next) {
+    res.render('signin', { title: 'Sign In' });
+  })
+  .post(function(req, res, next) {
+    console.log(req.body.accountid);
+    console.log(req.body.password);
+    res.render('signin', { title: 'Sign In' });
+  });
 
 module.exports = router;
